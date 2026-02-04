e0a89388fcbfb3e5a8c19acaab4e956216f29952
sureerat-2004 <66010914021@msu.ac.th>
2026-02-04 17:16:17 +0700

    er

--- h/connectdb.php
+++ h/connectdb.php
@@ -1,7 +1,7 @@
 <?php
 		$host = "localhost";
 		$user = "root";
-		$pwd = "";
+		$pwd = "66010914015";
 		$db = "4021db";
 		$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
 		mysqli_query($conn, "SET NAMES utf8");
