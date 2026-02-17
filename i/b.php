84b12c8d8ca8bbc70a2e98ff549a2e3bc3920750
sureerat-2004 <66010914021@msu.ac.th>
2026-02-18 00:10:27 +0700

    cx

--- i/b.php
+++ i/b.php
@@ -9,15 +9,7 @@
 
 <h1>งาน i --สุรีรัตน์ เกษกัน (เตย)</h1>
 
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
-<body>
 
-<h1>66010914015 ภาคภูมิ วรรณชัย (แมนนนน)</h1>
 
 <form method="post" action="" enctype="multipart/form-data">
     ชื่อจังหวัด<input type="text" name="pname" autofocus require><br>
