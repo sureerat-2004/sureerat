9347111114b31a812bd857f756545a384132aa3e
sureerat-2004 <66010914021@msu.ac.th>
2026-02-17 19:54:59 +0700

    cv

---
+++ i/b.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>สุรีรัตน์ เกษกัน (เตย)</title>
+</head>
+
+<body>
+
+<h1>งาน i --สุรีรัตน์ เกษกัน (เตย)</h1>
+
+<form method="post" action="">
+    ชื่อภาค <input type="text" name="rname" autofocus required>
+    <button type="submit" name="Submit">บันทึก</button>
+</form><br><br>
+
+<?php
+if(isset($_POST['Submit'])){
+    include_once("connectdb.php");
+    $rname = $_POST['rname'];
+    $sql2= "INSERT INTO `r_regions` (`r_id`, `r_name`) VALUES (NULL, '{$rname}')";
+    mysqli_query($conn, $sql2) or die ('เพิ่มข้อมู,ไม่ได้');
+}
+?>
+<table border="1">
+    <tr>
+        <th>รหัสจังหวัด</th>
+        <th>ชื่อจัวหวัด</th>
+        <th>ลบ</th>
+    </tr>
+<?php
+include_once("connectdb.php");
+$sql = "SELECT * FROM `provinces`";
+$rs = mysqli_query($conn, $sql);
+while ($data = mysqli_fetch_array($rs)){
+?>
+    <tr>
+        <td><?php echo $data['p_id'];?></td>
+        <td><?php echo $data['p_name'];?></td>
+        <td width="80" align="center"><a href="delete_regions.php?id=<img src=<?php echo $data['p_id'];?> onClick="return confirm('ยืนยันการลบ?');"><img src="images/delete.jpg"width="20"></a></td>
+    </tr>
+<?php } ?>
+</tadle>
+</body>
+</html>
+ 
+<?php
+ mysqli_close($conn)
+?>
