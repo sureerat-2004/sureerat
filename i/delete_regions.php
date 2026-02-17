b1a9651eddbf30f4d42e7a12da4cfcf10276a295
sureerat-2004 <66010914021@msu.ac.th>
2026-02-17 19:48:30 +0700

    lj

--- i/delete_regions.php
+++ i/delete_regions.php
@@ -1,12 +1,11 @@
 <meta charset="utf-8">
-?<php
+<?php
     include_once("connectdb.php");
 
     $sql = "DELETE FROM regions WHERE r_id='{$id}'";
     mysqli_query($conn, $sql) or die ("ลบข้อมูลไม่ได้");
 
     echo "<script>";
-    eaho "window.location=''a.php';"
+    echo "window.location='a.php';";
     echo "</script>";
-
 ?>
