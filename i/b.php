3fd4dc509d0713815d8bcd952c9335aa5c5e298b
sureerat-2004 <66010914021@msu.ac.th>
2026-02-18 00:09:27 +0700

    lp

--- i/b.php
+++ i/b.php
@@ -9,7 +9,17 @@
 
 <h1>งาน i --สุรีรัตน์ เกษกัน (เตย)</h1>
 
-<<form method="post" action="" enctype="multipart/form-data">
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<body>
+
+<h1>66010914015 ภาคภูมิ วรรณชัย (แมนนนน)</h1>
+
+<form method="post" action="" enctype="multipart/form-data">
     ชื่อจังหวัด<input type="text" name="pname" autofocus require><br>
     รูปประจำจังหวัด<input type="file" name="pimage" require><br>
     ภาค
@@ -32,6 +42,7 @@ while ($data3 = mysqli_fetch_array($rs3)){
 <?php
 if(isset($_POST['Submit'])){
     include_once("conectdb.php");
+    
 
     $pname = $_POST['pname'];
     $ext = pathinfo($_FILES['pimage']['name'], PATHINFO_EXTENSION);
@@ -42,16 +53,18 @@ if(isset($_POST['Submit'])){
     //var_dump($sql2);exit;
     $pid = mysqli_insert_id($conn);
     //copy($_FILES['pimage']['tmp_name'],"img/".$pid.".".$ext);
-    copy($_FILES['pimage']['tmp_name'],"images/".$qid.".".$ext);
+    move_uploaded_file($_FILES['pimage']['tmp_name'],"img/".$pid.".".$ext);
 }
 ?>
+
 <table border="1">
     <tr>
         <th>รหัสจังหวัด</th>
-        <th>ชื่อจัวหวัด</th>
+        <th>ชื่อจังหวัด</th>
         <th>ชื่อภาค</th>
-        <th>รูป</th>
+        <th>รูปจังหวัด</th>
         <th>ลบ</th>
+
     </tr>
 <?php
 include_once("conectdb.php");
@@ -60,7 +73,7 @@ $rs = mysqli_query($conn,$sql);
 while ($data = mysqli_fetch_array($rs)){
     
 ?>
-<<?php
+<?php
     include_once("conectdb.php");
     $sql = "SELECT * FROM `provinces` AS p INNER JOIN `regions` AS r ON p.r_id = r.r_id";
     $rs = mysqli_query($conn,$sql);
@@ -68,17 +81,17 @@ while ($data = mysqli_fetch_array($rs)){
 
 ?>
     <tr>
-        <td><?php echo $data['p_id'];?></td>
-        <td><?php echo $data['p_name'];?></td>
-        <td><?php echo $data['r_name'];?></td>
-        <td><img src="img/<?php echo $data['p_id'];?>.<?php echo $data['p_exp'];?>"width="140"></td>
+        <td><?php echo $data['p_id']; ?></td>
+        <td><?php echo $data['p_name']; ?></td>
+        <td><?php echo $data['r_name']; ?></td>
+        <td><img src="img/<?php echo $data['p_id']; ?>.<?php echo $data['p_ext']; ?>" width="80"></td>
         <td width="80" align="center"><a href="delete_province.php?id=<?php echo $data['p_id']; ?>$ext=<?php echo $data['p_ext'];?>"onClick="return confirm('ยืนยันการลบ?');"><img src="images/delete.jpg" width="80"></a></td>
     </tr>
 <?php } ?>
-</tadle>
+</table>
 </body>
 </html>
- 
 <?php
- mysqli_close($conn);
+mysqli_close($conn);
 ?>
+<?php } ?>
