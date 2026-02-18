688cc9fb7939e0d4dcb0bd87c43bdfe1b619a931
sureerat-2004 <66010914021@msu.ac.th>
2026-02-18 14:04:45 +0700

    vb

--- j/a.php
+++ j/a.php
@@ -1,20 +1,14 @@
 <!doctype html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>66010914021 สุรีรัตน์ เกษกัน (เตย)</title>
-    <style>
-        /* แถม CSS เล็กน้อยเพื่อให้รูปจัดวางสวยงาม */
-        .profile-img {
-            width: 200px; /* ปรับขนาดความกว้างตามต้องการ */
-            border-radius: 10px; /* ทำมุมโค้ง */
-            border: 2px solid #333;
-        }
-    </style>
+<meta charset="utf-8">
+<title>66010914021 สุรีรัตน์ เกษกัน (เตย)</title>
 </head>
 
 <body>
-    <h1>66010914021 สุรีรัตน์ เกษกัน (เตย)</h1>
-    <h1><img src="1.jpg"width="500"></h1>
+<h1>งาน j 66010914021 สุรีรัตน์ เกษกัน (เตย)</h1>
+<dr>
+<img src="1.jpg"width="500">
+
 </body>
 </html>
