8b6e4d98af6e48a6cf820820a86aeedb6f953dc1
sureerat-2004 <66010914021@msu.ac.th>
2026-02-18 13:58:17 +0700

    tr

--- j/a.php
+++ j/a.php
@@ -16,7 +16,7 @@
 <body>
     <h1>66010914021 สุรีรัตน์ เกษกัน (เตย)</h1>
     
-    <img src="images/my_photo.jpg" alt="1.images" class="profile-img">
+    <img src="images/my_photo.jpg" alt="1.jpg" class="profile-img">
 
 </body>
 </html>
