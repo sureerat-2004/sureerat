117e96760e33b61de6a3498350af50c6e257f272
sureerat-2004 <66010914021@msu.ac.th>
2026-02-18 17:04:13 +0700

    ed

--- k/a.php
+++ k/a.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>งาน K - สุรีรัตน์ เกษกัน </title>
+    <title>งาน K - สุรีรัตน์ เกษกัน เตย</title>
     <style>
         body {
             font-family: 'Arial', sans-serif;
